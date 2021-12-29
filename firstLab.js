bbe8e7071aa58f4e9efef03861cf12e8f6d27781
ibrahimhafez <ibrahim.hafez.abdallah@gmail.com>
2021-12-29 10:32:52 +0200

    files in the index

---
+++ firstLab.js
@@ -0,0 +1,18 @@
+// let result1 = prompt("Please enter your First Name");
+
+
+// console.log(result1);
+
+firstName = prompt("Please enter your First Name?", "");
+
+lastName = prompt("Please enter your Last Name?", "");
+
+birthYear = prompt("Enter your year of birth, please", "year of birth");
+
+let age = 2021 - Number(birthYear);
+
+let message = (`welcome ${firstName} ${lastName} you are ${age} years old`);
+
+confirm(message);
+
+alert(message);
---
+++ index2.js
@@ -0,0 +1,12 @@
+// window.alert("Welcome!");
+alert("It is our first release of a calculator that only has a summation feature.");
+
+num1 = prompt("Please enter your first number", "");
+
+num2 = prompt("Please enter your second number?", "");
+
+var result = Number(num1) + Number(num2);
+
+let message = (`Your result is ${result}`);
+
+alert(message)
